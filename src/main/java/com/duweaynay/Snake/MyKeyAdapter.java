bf2341a1cb34deb19d9a13374838b183f5279abc
Duwe Aynab <mfaynab88@gmail.com>
2022-06-26 22:18:12 -0400

    First commit

---
+++ src/main/java/com/duweaynay/Snake/MyKeyAdapter.java
@@ -0,0 +1,7 @@
+package com.duweaynay.Snake;
+
+import java.awt.event.KeyEvent;
+
+public interface MyKeyAdapter {
+    void KeyPressed(KeyEvent e);
+}
---
+++ src/main/java/com/duweaynay/Snake/SnakeApplication.java
@@ -0,0 +1,14 @@
+package com.duweaynay.Snake;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeApplication {
+
+	public static void main(String[] args) {
+
+         new GameFrame();
+	}
+
+}
